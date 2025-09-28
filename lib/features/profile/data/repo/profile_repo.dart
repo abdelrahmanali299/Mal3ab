b3720c21a7450723ef5c49dfbd0a2cb8c4f0e781
Moaz911gt <waleedmoaz65@gmail.com>
2025-09-28 16:57:04 +0300

    finish profile repo

--- lib/features/profile/data/repo/profile_repo.dart
+++ lib/features/profile/data/repo/profile_repo.dart
@@ -1,1 +1,8 @@
-abs
+import 'package:dartz/dartz.dart';
+import 'package:mal3ab/core/services/failure.dart';
+import 'package:mal3ab/features/auth/data/model/user_model.dart';
+
+abstract class ProfileRepo {
+  Future<Either<Failure, UserModel>> register();
+  Future<Either<Failure, void>> withdrow();
+}
